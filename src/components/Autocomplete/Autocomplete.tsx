af68c26a13be7ffdd468e04923bd3f6b6f84ca33
Lucas Rojas <lucas.rojas@live.com.ar>
2023-07-25 20:51:32 -0300

    Style changes

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -4,25 +4,44 @@ import { Film, asyncFilter, top100Films } from "../../api";
 
 const Autocomplete = () => {
 	const [filteredList, setFilteredList] = React.useState<Film[]>(top100Films);
-	const [inputValue, setInputValue] = React.useState<string>("");
-	const handleOnChange = async (ev: any) => {
+	const [inputValue, setInputValue] = React.useState<string | undefined>(
+		undefined
+	);
+
+	const handleOnChange = async (ev: React.ChangeEvent<HTMLInputElement>) => {
 		const query = ev.target.value;
 		setInputValue(query);
+
 		const value = await asyncFilter(query);
-		console.log("value", value);
+
 		setFilteredList(value);
 	};
+
+	const highlightText = (text: String, toHighlight: string) => {
+		// It should take in count the capitalization but I am not sure how to do it
+		const parts = text.split(toHighlight);
+		return (
+			<>
+				{parts[0]}
+				<span style={{ background: "yellow" }}>{toHighlight}</span>
+				{parts[1]}
+			</>
+		);
+	};
+
 	return (
 		<div className="autocomplete">
 			<input
 				className="autocomplete-input"
 				value={inputValue}
 				onChange={(ev) => handleOnChange(ev)}
-			/>
+			></input>
 			<div className="options-list">
 				<ul>
 					{filteredList.map((el: Film) => (
-						<li onClick={() => setInputValue(el.label)}>{el.label}</li>
+						<li onClick={() => setInputValue(el.label)}>
+							{inputValue ? highlightText(el.label, inputValue) : el.label}
+						</li>
 					))}
 				</ul>
 			</div>
